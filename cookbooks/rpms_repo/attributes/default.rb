3c87576d16bb92520116b1837dda597f3b80560d
George Lei <glei@storm8.com>
2017-02-10 16:25:33 -0800

    add rpms_repo

---
+++ cookbooks/rpms_repo/attributes/default.rb
@@ -0,0 +1,10 @@
+Dir.glob("/etc/yum.repos.d/*.repo").each do |f|
+  if File.readlines("#{f}").grep(/rpms.sv4/).size > 0
+    ruby_block 'update_rpms_repo' do
+      block do
+        fe = Chef::Util::FileEdit.new("#{f}")
+        fe.search_file_replace_line(/rpms/, 'rpms.sv4.prod.storm8.com')
+        fe.write_file
+      end
+  end
+end
